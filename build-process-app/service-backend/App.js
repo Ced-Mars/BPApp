afdce9f3d0f11d607df269c24616c960de47e2be
SOULARD Cédric <cedric.soulard.external@airbus.com>
2021-11-03 01:44:49 +0100

    Backend Alerts Data + Frontend Alert Modifications

--- build-process-app/service-backend/App.js
+++ build-process-app/service-backend/App.js
@@ -10,7 +10,6 @@ const server = http.createServer(app);
 
 app.use(express.static("dist"));
 //Global variables where are stored informations about the messaging server and the channel
-var connection, channel;
 var message = "Attente de la Recette";
 const server_path = "amqp://localhost";
 
@@ -26,73 +25,65 @@ main();
 
 //Calling main function in socketio-connection.js
 function main(){
-      //Connection avec socket.io pour communication avec le frontend
-      io.on("connection", (socket) => {
-        console.log("Client is connected");
-        socket.emit("FromBPAll", message);
-        console.log("emitted");
-
-        //Connexion to rabbitMQ server
-        try {
-          //Creating connection with rabbitMQ server
-          amqp.connect(server_path, function(error0, conn) {
-            if (error0) {
-              throw error0;
+  var exchange = 'sequencer';
+  //Connexion to rabbitMQ server
+  try {
+    //Creating connection with rabbitMQ server
+    amqp.connect(server_path, function(error0, connection) {
+      if (error0) {
+        throw error0;
+      }
+      connection.createChannel(function(error1, channel) {
+        if (error1) {
+          throw error1;
+        }
+        channel.assertExchange(exchange, 'topic', {
+          durable: false
+        });
+        channel.assertQueue('', {
+        }, function(error2, q) {
+          if (error2) {
+            throw error2;
+          }
+          console.log(' [*] Waiting for logs. To exit press CTRL+C');
+          key = 'buildp.all';
+          key2 = 'buildp.report';
+          channel.bindQueue(q.queue, exchange, key);
+          channel.bindQueue(q.queue, exchange, key2);
+          
+          channel.consume(q.queue, function(msg) {
+            // Emitting a new message. Will be consumed by the client
+            if(msg.fields.routingKey == key){
+              message = JSON.parse(msg.content);
+              socket.emit("FromBPAll", message);
+              console.log("%s message sent BPALL : ",msg.fields.routingKey, message);
+            }else if (msg.fields.routingKey == key2){
+                console.log("dans report");
+                socket.emit("FromBPAdv", JSON.parse(msg.content));
+                console.log("%s message sent : ",msg.fields.routingKey, JSON.parse(msg.content));
             }
-            connection = conn;
-            connection.createChannel(function(error1, chan) {
-              if (error1) {
-                throw error1;
-              }
-              channel = chan;
-              var exchange = 'sequencer';
-          
-              channel.assertExchange(exchange, 'topic', {
-                durable: false
-              });
-          
-              channel.assertQueue('', {
-              }, function(error2, q) {
-                if (error2) {
-                  throw error2;
-                }
-                console.log(' [*] Waiting for logs. To exit press CTRL+C');
-                key = 'buildp.all';
-                key2 = 'buildp.report';
-                channel.bindQueue(q.queue, exchange, key);
-                channel.bindQueue(q.queue, exchange, key2);
-                
-                channel.consume(q.queue, function(msg) {
-                  console.log("reçu : ", msg.content.toString());
-                  // Emitting a new message. Will be consumed by the client
-                  if(msg.fields.routingKey == key){
-                    message = JSON.parse(msg.content);
-                    socket.emit("FromBPAll", message);
-                    console.log("%s message sent BPALL : ",msg.fields.routingKey, message);
-                  }else if (msg.fields.routingKey == key2){
-                      console.log("dans report");
-                      socket.emit("FromBPAdv", JSON.parse(msg.content));
-                      console.log("%s message sent : ",msg.fields.routingKey, JSON.parse(msg.content));
-                  }
-                }, {
-                  noAck: true
-                });
+          }, {
+            noAck: true
+          });
+          //Connection avec socket.io pour communication avec le frontend
+          const socket = io.on("connection", (socket) => {
+            console.log("Client is connected");
+            socket.emit("FromBPAll", message);
+            console.log("emitted");
 
-              });
+            
+            //Called when the client disconnect from the socketio link
+            socket.on("disconnect", () => {
+              console.log("Client disconnected");
             });
           });
-          
-        } catch (e) {
-          console.error(e);
-        }
-        //Called when the client disconnect from the socketio link
-        socket.on("disconnect", () => {
-          channel.close();
-          connection.close();
-          console.log("Client disconnected");
         });
       });
-
-      server.listen(port, () => console.log(`Listening on port ${port}`));
+    });
+    
+  } catch (e) {
+    console.error(e);
+  }
+  server.listen(port, () => console.log(`Listening on port ${port}`));
 }
 
--- build-process-app/src/Display.js
+++ build-process-app/src/Display.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {List, ListItem, ListItemText, Card, CircularProgress } from "@mui/material";
+import {List, ListItem, ListItemText, Stepper, Step, StepButton, Button, Typography, CircularProgress, StepLabel } from "@mui/material";
 import "./Display.css";
 import socketIOClient from "socket.io-client";
 
@@ -14,6 +14,8 @@ export default function Display() {
   const [action, setAction] = useState("");
   const [statut, setStatut] = useState("");
   const [completedAction, setCompletedAction] = React.useState(new Set());
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [completedStep, setCompletedStep] = React.useState(new Set());
 
   
   useEffect(() => {
@@ -32,7 +34,17 @@ export default function Display() {
   
   if (response && response.length!==0 && response!== "Attente de la Recette") {
     console.log("dans render sequence");
-    return <RenderSequence props={response} action={action} statut={statut} completedAction={completedAction} setCompletedAction={setCompletedAction} />;
+    return <RenderSequence 
+    props={response} 
+    setProps={setResponse}
+    action={action} 
+    statut={statut} 
+    completedAction={completedAction} 
+    setCompletedAction={setCompletedAction}
+    activeStep={activeStep}
+    setActiveStep={setActiveStep}
+    completedStep={completedStep}
+    setCompletedStep={setCompletedStep} />;
   }
   return <RenderText props={response} />;
   
@@ -55,7 +67,22 @@ function RenderText({ props }) {
   );
 }
 
-function RenderSequence({ props, action, statut, completedAction, setCompletedAction }) {
+
+
+
+
+function RenderSequence({ 
+  props,
+  setProps,
+  action, 
+  statut, 
+  completedAction, 
+  setCompletedAction,
+  activeStep,
+  setActiveStep,
+  completedStep,
+  setCompletedStep 
+}) {
   const root = {
     flex:1,
     width: "100%",
@@ -83,10 +110,11 @@ function RenderSequence({ props, action, statut, completedAction, setCompletedAc
     margin:"5px",
     padding:"5px"
   };
+  const stepper = {
+    flex: "1 0 auto"
+  };
 
-  // Actions
-  
-
+  // Gestion Actions
   const handleCompleteAction = () => {
     console.log("dans handlecompleteaction", action);
     const newCompleted = new Set(completedAction);
@@ -99,20 +127,96 @@ function RenderSequence({ props, action, statut, completedAction, setCompletedAc
   };
 
   useEffect(() => {
-    //Ou action
-    console.log("dans useeffect action", isActionComplete(action));
     if(!isActionComplete(action)){
       handleCompleteAction();
     }
-    console.log("action set ",completedAction);
   }, [action]);
+
+  //Gestion Steps
+  const totalSteps = () => {
+    return props.length;
+  };
+
+  //return an array of string representing each step id and "End" string
+  function getSteps(props) {
+    var array = [];
+    props.map((value, index) => {
+      array = [...array, index+1];
+    })
+
+    return array;
+  }
+
+  function isStepComplete(step) {
+    return completedStep.has(step);
+  }
+
+  const completedSteps = () => {
+    return Object.keys(completedStep).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
+
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
+  };
+
+  const handleStep = (step) => () => {
+    setActiveStep(step);
+  };
+
+  const handleComplete = () => {
+    const newCompleted = completed;
+    newCompleted[activeStep] = true;
+    setCompletedStep(newCompleted);
+    handleNext();
+  };
+
+  //Reset everything
+  const handleReset = () => {
+    setActiveStep(0);
+    setCompletedStep(new Set());
+    setProps([]);
+    setCompletedAction(new Set());
+    setAction("");
+    setStatut("");
+  };
+
+  //Get back to the active (working) step/action
+  const handleFollow = () => {
+
+  }
+
+  
 
   return (
       <div style={root}>
+        <Stepper alternativeLabel nonLinear activeStep={activeStep}>
+          {getSteps(props).map((label, index) => {
+            return (
+              <Step key={index} completed={isStepComplete(index)}>
+                <StepButton
+                  focusRipple={true}
+                  onClick={handleStep(index)}
+                >
+                  <StepLabel>
+                    {label}
+                  </StepLabel>
+                </StepButton>
+              </Step>
+            );
+          })}
+        </Stepper>
         
         <div style={content}>
           <List style={list}>
-            {props.map((value, i, arr) => {
+            {props[activeStep].map((value, i, arr) => {
               return(
                   <ListItem button key={value["id"]} style={isActionComplete(value["id"]) ? {flex:1, backgroundColor:"lightgreen"} : {flex:1}}> 
                     <ListItemText primary={value["description"]}>
@@ -125,7 +229,16 @@ function RenderSequence({ props, action, statut, completedAction, setCompletedAc
         </div>
 
         <div style={footer}>
-          <p>Action en cours : {action} - Statut : {statut}</p>
+          {allStepsCompleted() ? (
+            <div className={classes.footerData}>
+              <Typography className={classes.instructions}>
+                Process has ended, you can reset it or launch a new process.
+              </Typography>
+              <Button onClick={handleReset}>Reset</Button>
+            </div>
+            ) : 
+            <p>Action en cours : {action} - Statut : {statut}</p>
+          }
         </div>
       </div>
   );
